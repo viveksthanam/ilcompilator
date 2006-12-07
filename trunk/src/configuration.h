2b6d471d9ac9aad679721328e3390eb0ff09d311
brossill.enseirb <brossill.enseirb@e8c33cc5-fd22-0410-9a52-6df4b5454074>
2006-12-07 16:52:43 +0000

    Ajout de configuration.h

---
+++ trunk/src/configuration.h
@@ -0,0 +1,12 @@
+// vim: ts=2 tw=80
+
+/** \file configuration.h
+ * \brief Contient les configurations des différents
+ * éléments du compilateur.
+ * \author <brossill@enseirb.fr> <lerouxj@enseirb.fr>
+ * \version
+ * \date 07/12/2006
+ *
+ */
+
+#define HASHTABLE_SIZE 100

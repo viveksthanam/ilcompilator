3bafd1bb06bffc2d64835cec60d43a57e123a093
brossill.enseirb <brossill.enseirb@e8c33cc5-fd22-0410-9a52-6df4b5454074>
2006-12-08 22:41:24 +0000

    Changement du container stl utilisé pour la pile de déclaration qui devien plus logiquement une file... et change ainsi de nom pour declarationqueue.{h,cpp}

--- trunk/src/type.h
+++ trunk/src/type.h
@@ -11,6 +11,17 @@
 #ifndef TYPE_H
 #define TYPE_H
 
+/**\brief typedef enum listant les différents types de base.
+  *
+  *\warning La position du type dans l'enumerate est importante
+  *car on suppose que la conversion en entier de l'enumerate
+  *permet de définir la règle suivante :
+  *Si TYPEA > TYPEB alors on peut convertir TYPEA en TYPEB 
+  *mais celà se fera avec perte de données.
+  *Si TYPEA < TYPEB alors la conversion de TYPEA vers TYPEB
+  * s'effectuera sans pertes.
+  *
+  */
 typedef enum _TYPEVAL
 {
   T_BOOL = 0,
@@ -23,16 +34,25 @@ typedef enum _TYPEVAL
 
 }TYPEVAL;
 
+/**\brief Classe implémentant le type d'une variable.
+  *
+  * Le type d'une variable est la conjonction entre le type
+  * de base de la variable (bool,int,float) et un entier
+  * représentant le niveau de référencement de cette variable.
+  */
 class CType
 {
 private:
 
+  /**\brief Le type de base de la variable */
   TYPEVAL typeval;
-
+  
+  /**\brief Le niveau de référencement de la variable */
   int ref_level;
 
 public:
 
+  /**\brief Constructeur par défault générant un type invalide*/
   CType()
   {
     this->typeval = T_INVALID;
@@ -44,10 +64,23 @@ public:
     this->typeval = typeval;
     this->ref_level = ref_level;
   }
-  
+ 
+  /**\brief Accesseur retournant le type de base */
   TYPEVAL getTypeVal() { return this->typeval; };
 
+  /**\brief Accesseur retournant le niveau de référencement */
   int getRef() { return this->ref_level; };
+
+  bool canConvertTo( CType type )
+  {
+    if( this->ref_level != type.ref_level ) return false;
+
+    if( (int)(this->typeval) < (int)(type.typeval) )
+      return true;
+    else
+      return false;
+  }
+  
 };
 
 #endif/*TYPE_H*/

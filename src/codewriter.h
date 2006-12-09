16e9fa9cfac57c7a2942dd2f836046de7c1cc7d2
brossill.enseirb <brossill.enseirb@e8c33cc5-fd22-0410-9a52-6df4b5454074>
2006-12-09 15:30:11 +0000

    Update du code dans CCodeWriter, ne fait rien mais compile

--- src/codewriter.h
+++ src/codewriter.h
@@ -13,6 +13,9 @@
 #ifndef CODEWRITER_H
 #define CODEWRITER_H
 
+#include "instructionqueue.h"
+#include "declarationqueue.h"
+
 /**\brief Classe chargée d'écrire le code 3@ et de manipuler le
   * fichier de sortie.
   */
@@ -24,10 +27,30 @@ private:
 
 public:
 
+  /**\brief Constructeur par défault, ouvre le fichier
+    *DEFAULT_OUTPUT_FILENAME en écriture.
+    */
   CCodeWriter();
-  CCodeWriter(char*);
+
+  /**\brief Constructeur par défault, ouvre le fichier
+    *passé en argument en écriture.
+    *\param filename Fichier à ouvrir.
+    */
+  CCodeWriter(char* filename);
+
   ~CCodeWriter();
 
+  /**\brief Ecrit dans le fichier les instructions de déclaration.
+    *\param declarations Pile des déclarations à vider 
+    *pour remplir le fichier de sortie.
+    */
+  int writeDeclarations( CDeclarationQueue* declarations );
+
+  /**\brief Ecrit dans le fichier les instructions. 
+    *\param instructions Pile des instructions à vider 
+    *pour remplir le fichier de sortie.
+    */
+  int writeInstructions( CInstructionQueue* instructions );
 };
 
 #endif/*CODEWRITER_H*/
